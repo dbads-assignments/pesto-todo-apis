d4a742ed63ae824e397c2537c029a5b4fbd93014
dbads <deepakbharti@mnnit.ac.in>
2024-05-23 18:38:48 +0530

    completed apis

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todo');
+const cors = require('cors');
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+  };
+
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 let dbUrl = 'mongodb+srv://todos:todos@assignments-cluster.uesk9xg.mongodb.net/?retryWrites=true&w=majority&appName=assignments-cluster'
